1b8321598116bc7e8d9f12a1ca813a3cde9366a5
bretto36 <brett@sitementor.com.au>
2016-06-24 00:38:40 -0400

    Added correct query builder response to form type

--- src/AppBundle/Repository/SlotRepository.php
+++ src/AppBundle/Repository/SlotRepository.php
@@ -13,15 +13,28 @@ use Doctrine\ORM\EntityRepository;
 class SlotRepository extends EntityRepository
 {
     /**
+     * @param $startTime
+     * @param $endTime
+     *
      * @return Slot[]
      */
-    public function findAllSlotsAvailable($startTime, $endTime)
+    public function queryAllSlotsAvailable($startTime, $endTime)
     {
         return $this->createQueryBuilder('slot')
             ->andWhere('slot.start_time < :startTime AND slot.end_time < :startTime')
             ->orWhere('slot.start_time > :endTime AND slot.end_time > :endTime')
             ->setParameter('startTime', $startTime)
-            ->setParameter('endTime', $endTime)
+            ->setParameter('endTime', $endTime);
+    }
+    /**
+     * @param $startTime
+     * @param $endTime
+     *
+     * @return Slot[]
+     */
+    public function findAllSlotsAvailable($startTime, $endTime)
+    {
+        return $this->queryAllSlotsAvailable($startTime, $endTime)
             ->getQuery()
             ->execute();
     }
